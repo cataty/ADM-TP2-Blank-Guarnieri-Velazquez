0e5e898b759a8fa5db9debef86d02d43dd5a9463
rayitoguarnieri <128533157+rayitoguarnieri@users.noreply.github.com>
2024-06-21 15:24:37 -0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -5,32 +5,40 @@
  */
 
 // Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
+import { createRouter, createWebHistory } from 'vue-router'
 import { setupLayouts } from 'virtual:generated-layouts'
-import index from '@/pages/index.vue'
-import movie from '@/pages/movie.vue'
-import favourites from '@/pages/favourites.vue'
 
+// Importar componentes de las páginas
+import Index from '@/pages/index.vue'
+import Movie from '@/pages/movie.vue'
+import Favourites from '@/pages/favourites.vue'
+import MovieDetail from '@/components/MovieDetail.vue'
+
+// Configuración de rutas
+const routes = setupLayouts([
+  {
+    path: '/',
+    name: 'home',
+    component: Index
+  },
+  {
+    path: '/movie/:id',
+    name: 'movie',
+    component: MovieDetail
+  },
+  {
+    path: '/favourites',
+    name: 'favourites',
+    component: Favourites
+  },
+  // Otras rutas...
+])
+
+// Crear instancia del router
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  extendRoutes: setupLayouts,
-  routes:[
-    {
-      path: '/',
-      name: 'home',
-      component: index
-    },
-    {
-      path: '/movie/:id',
-      name: 'movie',
-      component: movie
-    },
-    {
-      path: '/favourites',
-      name: 'favourites',
-      component: favourites
-    },
-    ]
+  routes,
 })
 
 export default router
+
