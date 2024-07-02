4d3d2a122e399a5081f85ac66f0c1e2ee673ee68
cataty <tatjana.blank@yahoo.de>
2024-07-02 14:26:10 -0300

    feat: agregar favicon, title y logo

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <v-app>
     <v-app-bar :elevation="2" class="navbar">
       <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
-      <v-app-bar-title>MyMovieFavs</v-app-bar-title>
+      <img src="/src/assets/mymovieApp-logo.png" alt="logo" class="logo">
+      <v-app-bar-title class="title">MyMovieFavs</v-app-bar-title>
       <router-link to="/" class="nav-link"><v-btn text="Home"</v-btn></router-link>
       <router-link to="/favourites" class="nav-link"><v-btn text="Favourites"</v-btn></router-link>
     </v-app-bar>
@@ -21,9 +22,19 @@
 <style>
 .navbar{
   background:#f8f8fc !important;
+  padding:0 2rem;
+}
+
+.title{
+  color:#0D47A1!important;
 }
 
 .nav-link{
   color:#1565C0 !important;
+}
+
+.logo{
+  height:40px;
+  width: auto;
 }
 </style>
